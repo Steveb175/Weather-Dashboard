3034313ad0547b7561a203e867b33b69b9bca1d0
Stephen Bondurant <stephenbondurant2014@gmail.com>
2023-03-18 13:11:23 -0500

    successfully connected API data to the current weather

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,10 @@
-// var searchButton = document.querySelector(".city-search");
+// Global variables for current weather
+var searchButton = document.querySelector(".city-search");
+var currentCityWeather = document.querySelector("#current-city-weather");
+var currentCityName = document.querySelector("#current-city-name");
+var currentTemp = document.querySelector("#current-temp");
+var currentWind = document.querySelector("#current-wind");
+var currentHumidity = document.querySelector("#current-humidity");
 
 // API key
 var apiKey = "f99598aacae7785a52e71cedad2a806e";
@@ -9,12 +15,30 @@ var requestUrl =
   "https://api.openweathermap.org/data/2.5/weather?q=" +
   city +
   "&appid=" +
-  apiKey;
+  apiKey +
+  "&units=imperial";
 // function searchCity() {
 //   var urlCurrentDay =
 //     "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
 // }
+function getApi(requestUrl) {
+  fetch(requestUrl)
+    .then(function (response) {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      currentCityName.textContent = currentTemp.textContent =
+        "Temp: " + data.main.temp + "°F";
+      currentWind.textContent = "Wind: " + data.wind.speed;
+      currentHumidity.textContent = "Humidity: " + data.main.humidity;
+      console.log(data);
+    })
+    .catch(function (error) {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+}
 
-fetch(requestUrl)
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+getApi(requestUrl);
